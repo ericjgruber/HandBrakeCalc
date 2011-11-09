821a8dc8b0112d691fe67607d4f9f25fde9b5819
Eric J. Gruber <eric@rumblestrut.com>
2011-11-08 21:04:33 -0600

    Added the .rb file.

---
+++ handcalc.rb
@@ -0,0 +1,12 @@
+puts 'Enter the number of hours.'
+hours = gets.to_i
+
+puts 'Now, enter the number of minutes.'
+minutes = gets.to_i
+
+puts 'Finally, enter the number of seconds.'
+seconds = gets.to_i
+
+total = (((hours * 60) + minutes) * 60) + seconds
+
+puts 'Your total is ' + total.to_s + '.'
